a984827a87ec1eaec2a0d90e6988859feac9433f
luizawander <luizasalesgithub@gmail.com>
2024-01-06 18:04:50 -0300

    Estrutura switch case
    base da estrutura que depois será aprimorada com métodos.

--- chatbot_w/Mensagens.cs
+++ chatbot_w/Mensagens.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,12 @@ namespace chatbot_w
 {
     public class Mensagens
     {
-        public void Iniciar(){
+        public void Iniciar()
+        {
             while (true)
             {
+                int Escolha;
+
                 Console.WriteLine("Olá. Você está no atendimento online da ZapZum Fibra Óptica.");
                 Console.WriteLine("Para começarmos, escolha uma das opções abaixo: ");
                 Console.WriteLine("1: Preciso de suporte técnico");
@@ -17,6 +22,38 @@ namespace chatbot_w
                 Console.WriteLine("3: Desejo alterar ou cancelar meu plano");
                 Console.WriteLine("4: Ainda não sou cliente e desejo me informar sobre os planos");
                 Console.WriteLine("5: Sair");
+
+                if (int.TryParse(Console.ReadLine(), out Escolha))
+                {
+                    switch (Escolha)
+                    {
+                    case 1:                    
+                        Console.WriteLine("..."); // depois encaixar os métodos certos
+                        break;                    
+
+                    case 2:                   
+                        Console.WriteLine("..."); // depois encaixar os métodos certos
+                        break;
+
+                    
+
+                    case 3:
+                        Console.WriteLine("..."); // depois encaixar os métodos certos
+                        break;
+                    
+
+                    case 4:
+                    Console.WriteLine("..."); // depois encaixar os métodos certos
+                        break;
+            
+
+                    case 5:
+                        Console.WriteLine("..."); // depois encaixar os métodos certos
+                        return;
+                    
+             
+                    }
+                }
             }
         }
     }
