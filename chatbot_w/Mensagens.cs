c4ac553a441d04b513c32f5559fcd942a276f594
luizawander <luizasalesgithub@gmail.com>
2024-01-08 18:17:51 -0300

    case 2

--- chatbot_w/Mensagens.cs
+++ chatbot_w/Mensagens.cs
@@ -71,49 +71,68 @@ namespace chatbot_w
 
                 Console.WriteLine($"{NomeCliente}, escolha uma das opções abaixo: " );
                 Console.WriteLine("1: Minha internet não está funcionando.");
-                Console.WriteLine("2: Problemas de velocidade de internet.");
-                Console.WriteLine("3: Meu roteador não liga.");
-                Console.WriteLine("4: Identifiquei que meu fio está rompido.");
-                Console.WriteLine("5: Desejo mudar a senha da minha rede.");
-                Console.WriteLine("6: Desejo falar com um atendente da Zapzum");
-                Console.WriteLine("7: Sair");
+                Console.WriteLine("2: Meu roteador não liga.");
+                Console.WriteLine("3: Identifiquei que meu fio está rompido.");
+                Console.WriteLine("4: Desejo mudar a senha da minha rede.");
+                Console.WriteLine("5: Desejo falar com um atendente da Zapzum");
+                Console.WriteLine("6: Sair");
 
                 if (int.TryParse(Console.ReadLine(), out Esc1))
                 {
-                     switch (Esc1)
-                     {
-                         case 1:
+                   switch (Esc1)
+                   {
+                        case 1:
                             Console.WriteLine("Primeiro, tente reiniciar seu roteador ao menos 2 vezes.");
                             Console.WriteLine("Se o problema persistiu, digite SIM ");
                             string Resp = Console.ReadLine();
-                         // verificar depois se caso a resposta for dada em letra minuscula, isso irá dar erro na execução.
-                         if (Resp == "SIM")
-                        {
-                             Console.WriteLine("Certo. Iremos agendar um atendimento presencial de um técnico da ZapZum.");
-                                //aqui fazer outra ver na TBL para informar escrever o endereço do cliente
+                            // verificar depois se caso a resposta for dada em letra minuscula, isso irá dar erro na execução.
+                            if (Resp == "SIM")
+                            {
+                                Console.WriteLine("Certo. Iremos agendar um atendimento presencial de um técnico da ZapZum.");
+                                   //aqui fazer outra verificação na TBL para informar escrever o endereço do cliente
                                 Console.WriteLine ("Confirme seu endereço digitando SIM :--puxar o endereço na tbl--");
                                 string Resp2=Console.ReadLine();
 
                                  if(Resp2=="SIM")
-                                {
-                                    Console.WriteLine("Ok. Em até 48 horas um dos nossos técnicos irá visitar sua residência.");
-                                    Console.WriteLine("Certifique-se que tenha uma pessoa durante os nosso horário de atendimento (9h-18h).");
-                                    Console.WriteLine("Além disso, nosso atendimento só é feito mediante a presença de um responsável maior de idade");
-                                    break;
+                               {
+                                    Console.WriteLine("Ok. Em até 48 horas um dos nossos técnicos irá fazer a visita técnica");
+                                    Console.WriteLine("Certifique-se que tenha uma pessoa em sua residência durante os nossos horário de atendimento (9h-18h).");
+                                    Console.WriteLine("Além disso, nosso atendimento só será feito mediante a presença de um responsável maior de idade");
+                                    Environment.Exit(0);
                                 }
 
                                  else 
                                 {
                                     Console.WriteLine("Certo, parece que temos um erro com seus dados. Iremos te encaminhar para o atentimento com um dos nossos técnicos. Aguarde um minuto");
                                 }
-                        }
-                        else if (Resp == "NÃO")
-                        {
-                            Console.WriteLine("Ok. Encerramos então nosso atendimento. Tenha um ótimo dia.");
-                            break;
-                        }                   
-                         break;
-                    }
+                            }
+
+                            else if (Resp == "NÃO")
+                            {
+                                Console.WriteLine("Ok. Encerramos então nosso atendimento. Tenha um ótimo dia.");
+                                Environment.Exit(0);
+                            }     
+                                break;
+
+                        case 2:
+                            Console.WriteLine("Certo. Primeiro verifique se todos os cabos estão conectatos de forma correta no roteador.");
+                            Console.WriteLine("Além disso, também verifique se ele está devidamente ligado em uma tomada que funcione.");
+                            Console.WriteLine("Caso você tenha feito tudo isso e ainda sim o problema persistiu, digite SIM");
+                            string Resp3 = Console.ReadLine();
+                            if(Resp3=="SIM")
+                               {
+                                    Console.WriteLine("Ok. Em até 48 horas um dos nossos técnicos irá fazer a visita técnica.");
+                                    Console.WriteLine("Certifique-se que tenha uma pessoa em sua residência durante os nossos horário de atendimento (9h-18h).");
+                                    Console.WriteLine("Além disso, nosso atendimento só será feito mediante a presença de um responsável maior de idade.");
+                                    Environment.Exit(0);
+                                }
+                            else if (Resp3 == "NÃO")
+                            {
+                                Console.WriteLine ("Ok. Encerramos então nosso atendimento. Tenha um ótimo dia.");
+                                Environment.Exit(0);
+                            }
+                        break;
+                    } 
                 } 
             }  // CHAVE WHILE  
         }
