04b3c1bcab70955ca345cc00e6c0a2ded3766823
luizawander <luizasalesgithub@gmail.com>
2024-01-08 18:19:09 -0300

    case 2
    adicionando o case 2, que se trata sobre problemas com o roteador.

--- chatbot_w/Program.cs
+++ chatbot_w/Program.cs
@@ -1,2 +1,13 @@
 ﻿using System;
 
+namespace chatbot_w
+{
+    class Program
+    {
+        static void Main()
+        {
+            Mensagens chatbot = new Mensagens();
+            chatbot.Iniciar();
+        }
+    }
+}
