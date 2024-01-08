aef7099a18d9a86b8b359bf0f8834bfbb095c866
luizawander <luizasalesgithub@gmail.com>
2024-01-08 11:48:39 -0300

    Estrutura do Suporte_Tec completa

--- chatbot_w/Mensagens.cs
+++ chatbot_w/Mensagens.cs
@@ -65,7 +65,58 @@ namespace chatbot_w
             Console.WriteLine ("Para comerçamos, digite seu nome completo sem abreviações");
             NomeCliente=Console.ReadLine();
             // aqui vou adicionar um if para verificar se o cliente realmente existe na tabela MySQL
+            while (true)
+            {
+                int Esc1;
+
+                Console.WriteLine($"{NomeCliente}, escolha uma das opções abaixo: " );
+                Console.WriteLine("1: Minha internet não está funcionando.");
+                Console.WriteLine("2: Problemas de velocidade de internet.");
+                Console.WriteLine("3: Meu roteador não liga.");
+                Console.WriteLine("4: Identifiquei que meu fio está rompido.");
+                Console.WriteLine("5: Desejo mudar a senha da minha rede.");
+                Console.WriteLine("6: Desejo falar com um atendente da Zapzum");
+                Console.WriteLine("7: Sair");
+
+                if (int.TryParse(Console.ReadLine(), out Esc1))
+                {
+                     switch (Esc1)
+                     {
+                         case 1:
+                            Console.WriteLine("Primeiro, tente reiniciar seu roteador ao menos 2 vezes.");
+                            Console.WriteLine("Se o problema persistiu, digite SIM ");
+                            string Resp = Console.ReadLine();
+                         // verificar depois se caso a resposta for dada em letra minuscula, isso irá dar erro na execução.
+                         if (Resp == "SIM")
+                        {
+                             Console.WriteLine("Certo. Iremos agendar um atendimento presencial de um técnico da ZapZum.");
+                                //aqui fazer outra ver na TBL para informar escrever o endereço do cliente
+                                Console.WriteLine ("Confirme seu endereço digitando SIM :--puxar o endereço na tbl--");
+                                string Resp2=Console.ReadLine();
+
+                                 if(Resp2=="SIM")
+                                {
+                                    Console.WriteLine("Ok. Em até 48 horas um dos nossos técnicos irá visitar sua residência.");
+                                    Console.WriteLine("Certifique-se que tenha uma pessoa durante os nosso horário de atendimento (9h-18h).");
+                                    Console.WriteLine("Além disso, nosso atendimento só é feito mediante a presença de um responsável maior de idade");
+                                    break;
+                                }
+
+                                 else 
+                                {
+                                    Console.WriteLine("Certo, parece que temos um erro com seus dados. Iremos te encaminhar para o atentimento com um dos nossos técnicos. Aguarde um minuto");
+                                }
+                        }
+                        else if (Resp == "NÃO")
+                        {
+                            Console.WriteLine("Ok. Encerramos então nosso atendimento. Tenha um ótimo dia.");
+                            break;
+                        }                   
+                         break;
+                    }
+                } 
+            }  // CHAVE WHILE  
         }
     } // chave public class Suporte_Tec
 
-}// chave namespace
+} // chave namespace
