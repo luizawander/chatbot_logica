74228487a7ceaaf50b6a83810ac256c120fb7e90
luizawander <luizasalesgithub@gmail.com>
2024-01-09 17:35:36 -0300

    Organizações.
    Para o código ficar mais organizado, separei a classe Suporte técnico e resolvi criar uma nova página para cada nova Classe/Função. Além de coloca-los dentro de uma pasta intitulada de "Função"

--- chatbot_w/função/Suporte_T.cs
+++ chatbot_w/função/Suporte_T.cs
@@ -1,59 +1,11 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data.Common;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace chatbot_w
 {
-    public class Mensagens
-    {
-        public void Iniciar()
-        {
-            while (true)
-            {
-                int Escolha;
-
-                Console.WriteLine("Olá. Você está no atendimento online da ZapZum Fibra Óptica.");
-                Console.WriteLine("Para começarmos, escolha uma das opções abaixo: ");
-                Console.WriteLine("1: Preciso de suporte técnico");
-                Console.WriteLine("2: Preciso de suporte financeiro");
-                Console.WriteLine("3: Desejo alterar ou cancelar meu plano");
-                Console.WriteLine("4: Ainda não sou cliente e desejo me informar sobre os planos");
-                Console.WriteLine("5: Sair");
-
-                if (int.TryParse(Console.ReadLine(), out Escolha))
-                {
-                    switch (Escolha)
-                    {
-                    case 1:                    
-                        new Suporte_Tec().Executar();
-                        break;                    
-
-                    case 2:                   
-                        Console.WriteLine("..."); // depois encaixar os métodos certos
-                        break;
-
-                    case 3:
-                        Console.WriteLine("..."); // depois encaixar os métodos certos
-                        break;
-                    
-                    case 4:
-                    Console.WriteLine("..."); // depois encaixar os métodos certos
-                        break;
-            
-                    case 5:
-                        Console.WriteLine("..."); // depois encaixar os métodos certos
-                        return;
-                    }
-                }
-            }
-        }
-    }
-
-    public class Suporte_Tec
+        public class Suporte_Tec
     {
         public void Executar()
         {
@@ -175,5 +127,4 @@ namespace chatbot_w
             }  // CHAVE WHILE  
         }
     } // chave public class Suporte_Tec
-
-} // chave namespace
+}
